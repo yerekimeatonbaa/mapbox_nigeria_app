e2738224d6657769c208ccc4c534aecc6e0d5dfb
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2025-09-17 23:43:01 +0100

    Add new file

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,22 +1,26 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-// ADD THESE LINES RIGHT HERE:
-val flutterVersionCode = property("flutter.versionCode").toString().toIntOrNull() ?: 1
-val flutterVersionName = property("flutter.versionName").toString()
-val flutterCompileSdkVersion = property("flutter.compileSdkVersion").toString().toIntOrNull() ?: 34
-val flutterMinSdkVersion = property("flutter.minSdkVersion").toString().toIntOrNull() ?: 21
-val flutterTargetSdkVersion = property("flutter.targetSdkVersion").toString().toIntOrNull() ?: 34
-val flutterNdkVersion = property("flutter.ndkVersion").toString()
+val flutterProperties = Properties().apply {
+    load(File(project.rootDir, "flutter.properties").inputStream())
+}
+
+val flutterVersionCode = flutterProperties.getProperty("flutter.versionCode")?.toIntOrNull() ?: 1
+val flutterVersionName = flutterProperties.getProperty("flutter.versionName") ?: "1.0"
+val flutterCompileSdkVersion = flutterProperties.getProperty("flutter.compileSdkVersion")?.toIntOrNull() ?: 34
+val flutterMinSdkVersion = flutterProperties.getProperty("flutter.minSdkVersion")?.toIntOrNull() ?: 21
+val flutterTargetSdkVersion = flutterProperties.getProperty("flutter.targetSdkVersion")?.toIntOrNull() ?: 34
+val flutterNdkVersion = flutterProperties.getProperty("flutter.ndkVersion") ?: ""
 
 android {
     namespace = "com.example.mapbox_nigeria_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = flutterCompileSdkVersion
+    ndkVersion = flutterNdkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -28,20 +32,16 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.mapbox_nigeria_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = flutterMinSdkVersion
+        targetSdk = flutterTargetSdkVersion
+        versionCode = flutterVersionCode
+        versionName = flutterVersionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            isMinifyEnabled = false
             signingConfig = signingConfigs.getByName("debug")
         }
     }
